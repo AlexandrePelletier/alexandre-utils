6b3adf452c8fb59ce2bd8fe62ff0903a7618d94f
Alexandre Pelletier <45462633+AlexandrePelletier@users.noreply.github.com>
2023-11-02 16:43:28 -0400

    Update getting_started.R

--- getting_started.R
+++ getting_started.R
@@ -15,6 +15,7 @@ emmaplot(res_fgsea_top)
 #you can reduce the pathway label size
 emmaplot(res_fgsea_top,label.size = 2)
 
+
 #to display more variety of pathways (coming from different source),the results can be prefiltered for
 #top pathways by pathways source (subcategory)
 res_gsea_topbycat<-res_fgsea_top[padj<0.05, #select only pathways those with padj<0.05
@@ -26,6 +27,10 @@ emmaplot(head(res_gsea_topbycat[order(pval)],80), #subset the top80 pathways acc
 
 #this is a ggplot format so the plot can be saved using
 ggsave('examples_data/emmaplot_top80_fgsea_apoe22_vs_33_microglia.pdf')
+
+
+#[New] You can decided to display only the pathway of a certain gene (This gene is in the leading edge genes of the pathway)
+emmaplot(res_fgsea_top,show_pathway_of = 'APOE4')
 
 
 
