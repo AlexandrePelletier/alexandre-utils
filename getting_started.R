6097a84bdae4facf5ef08df46cc41a3d75d7f407
Alexandre Pelletier <45462633+AlexandrePelletier@users.noreply.github.com>
2023-11-02 16:38:51 -0400

    Update and rename examples_script.R to getting_started.R

--- getting_started.R
+++ getting_started.R
@@ -2,7 +2,7 @@
 library(data.table)
 source('visualisation.R')
 
-#Network visualisation of fgsea results using emmaplot function####
+#Network visualisation of fgsea results using emmaplot function ####
 #load fgsea results
 res_fgsea<-fread('examples_data/res_fgsea_apoe22_vs_33_microglia.csv.gz')
 
@@ -11,16 +11,21 @@ res_fgsea<-fread('examples_data/res_fgsea_apoe22_vs_33_microglia.csv.gz')
 res_fgsea_top<-head(res_fgsea[order(pval)],80)
 
 emmaplot(res_fgsea_top)
+
 #you can reduce the pathway label size
 emmaplot(res_fgsea_top,label.size = 2)
 
 #to display more variety of pathways (coming from different source),the results can be prefiltered for
 #top pathways by pathways source (subcategory)
 res_gsea_topbycat<-res_fgsea_top[padj<0.05, #select only pathways those with padj<0.05
-                                 .SD[head(order(padj),30)],
+                                 .SD[head(order(padj),30)], #filter the data.table by source subcategory by keeping only the top30 by subcat
                                  by='subcat']
 
-emmaplot(head(res_gsea_topbycat[order(pval)],80),label.size = 2)
+emmaplot(head(res_gsea_topbycat[order(pval)],80), #subset the top80 pathways according to the pval of the enrichment
+         label.size = 2)
+
 #this is a ggplot format so the plot can be saved using
 ggsave('examples_data/emmaplot_top80_fgsea_apoe22_vs_33_microglia.pdf')
+
+
 
