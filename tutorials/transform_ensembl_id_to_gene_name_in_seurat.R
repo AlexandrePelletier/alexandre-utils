a1bdb53788416652051e76161ff6d83a55006716
Alexandre Pelletier <45462633+AlexandrePelletier@users.noreply.github.com>
2023-12-08 13:26:19 -0500

    Update transform_ensembl_id_to_gene_name_in_seurat.R

--- tutorials/transform_ensembl_id_to_gene_name_in_seurat.R
+++ tutorials/transform_ensembl_id_to_gene_name_in_seurat.R
@@ -6,8 +6,8 @@ library(Seurat)
 s1<-readRDS('/projectnb/tcwlab/LabMember/akg/APOE_Jorganoid_project/outputs/simpleaf/sample1.rds')
 
 head(rownames(s1))
-# [1] "ENSG00000268020.3"  "ENSG00000240361.1"  "ENSG00000186092.4"  "ENSG00000248527.1"  "ENSG00000228794.8" 
-# [6] "ENSG00000188157.14"
+# [1] "ENSG00000268020"  "ENSG00000240361"  "ENSG00000186092"  "ENSG00000248527"  "ENSG00000228794" 
+# [6] "ENSG00000188157"
 
 #translation betweem ensembl and hgnc symbok
 genes_trans<-fread('/projectnb/tcwlab/LabMember/adpelle1/projects/APOE_Jorganoid/outputs/CellRangerCount/sample_1/outs/filtered_feature_bc_matrix/features.tsv.gz',select = 1:2,col.names = c('gene_id','hgnc_symbol'))
@@ -26,13 +26,13 @@ genes_trans<-fread('/projectnb/tcwlab/LabMember/adpelle1/projects/APOE_Jorganoid
 # 36600: ENSG00000277196  AC007325.2
 
 
-have.gene_name<-!is.na(genes_trans[str_remove(rownames(s1),'.[0-9]+$'),on='gene_id']$hgnc_symbol)
+have.gene_name<-!is.na(genes_trans[rownames(s1),on='gene_id']$hgnc_symbol)
 sum(have.gene_name) #34027/36600 have gene names
 
 s1<-s1[have.gene_name,]
 
 mat<-s1[['RNA']]$counts
-rownames(mat)<-genes_trans[str_remove(rownames(mat),'.[0-9]+$'),on='gene_id']$hgnc_symbol
+rownames(mat)<-genes_trans[rownames(mat),on='gene_id']$hgnc_symbol
 
 sum(duplicated(rownames(mat)))#7 duplicated genes
 
